7c7d0f4f6dd2e3ffe44a10b3d19acc4a7909250d
Niklas Rosenstein <rosensteinniklas@gmail.com>
2018-02-26 15:12:41 +0100

    Update About dialog

--- c4d_prototype_converter/ui/HelpMenu.py
+++ c4d_prototype_converter/ui/HelpMenu.py
@@ -1,6 +1,7 @@
 
 import c4d
 import nr.c4d.ui
+import textwrap
 import webbrowser
 
 
@@ -14,7 +15,14 @@ class HelpMenu(nr.c4d.ui.Component):
     self['report'].add_event_listener('click', self._on_report)
 
   def _on_about(self, item):
-    c4d.gui.MessageDialog('C4D Prototype Converter')
+    c4d.gui.MessageDialog(textwrap.dedent('''
+      C4D Prototype Converter
+
+      Programming and Design: Niklas Rosenstein
+      Concept and Design: Donovan Keith
+
+      This project was sponsored by Maxon US.
+    ''').strip())
 
   def _on_help(self, item):
     webbrowser.open('https://github.com/NiklasRosenstein/c4d-prototyping-converter/wiki')
